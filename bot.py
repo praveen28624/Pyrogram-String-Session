b59ac08a15a770b5b8ad57a3bfa7a72485fd4535
praveen28624 <68008375+praveen28624@users.noreply.github.com>
2021-03-22 21:08:55 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,37 @@
+import time
+from pyrogram import Client
+import tgcrypto
+
+select = " "
+
+tutor = """
+~ go-to my.telegram.org
+~ Login using your Telegram account
+~ Click on API Development Tools
+~ Create a new application, by entering the required details
+"""
+
+template = """
+UserBot support: @userbotindo
+            
+<code>STRING_SESSION</code>: <code>{}</code>
+
+⚠️ <b>Please be carefull to pass this value to third parties</b>"""
+
+print("""\nRunning script...""")
+time.sleep(1)
+print(tutor)
+
+app_id=2940667
+api_hash="8590c88aca3638eb321979577ddb53d3"
+phone_number="+94772693488"
+
+app = Client("my_account",app_id,api_hash,phone_number=phone_number)
+
+
+with app:
+  saved_messages_template = "Pyrogram session" + template.format(app.export_session_string())
+  print("\nGenerating String session...\n")           
+  app.send_message("me",saved_messages_template,parse_mode="html")
+  time.sleep(1) 
+  print("Your STRING_SESSION value have been sent to your Telegram Saved Messages")
